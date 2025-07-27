56b069e12dce05faf858ab3b9baa60eeab872ed0
Doszhan <doszhan@MacBook-Air-Doszhan.local>
2025-07-27 22:59:18 +0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const WeatherPage(), // Мы скоро создадим WeatherPage
+     // home: const WeatherPage(), 
     );
   }
 }
